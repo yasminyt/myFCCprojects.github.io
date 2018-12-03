6ece1d2b73a4eec8feb179a7c6b30d2e6e72f915
yasminyt <544236589@qq.com>
2018-12-03 22:30:26 +0800

    Basically solve the possible abnormal inputs
    # Possible abnormal inputs
    1. Repeat input multiple 0s at the beginning
    2. Continuous input operator
    3. Click the '=' directly after pressing the operator
    4. Use the result of the last operation to perform a new operation
    5. Click 0 first and then click on the non-zero number
    6. Enter a number and press the '=' directly
    ...

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,69 +1,100 @@
 let values = [], data = [], operation = [], tmp = []
-let prevData = '', clickEqual = false 
+let prevData = false, result = false, err = false
+
+const abnormal = {
+  AddZero: 'No Number but Operation',   //一开始没有按数字而直接点了操作符的情况
+  IgnoreOperation: 'Operation and Equal', //按了操作符后直接按等号
+  ContinuousOperation: 'Continuous Input Operation', //连续输入了操作符
+  DivisorIs0: 'Divisor is 0', //除数为0
+}
 
 $(function () {
   $('input[type=button]').click((e) => {
     const clickValue = e.target.value
     if (clickValue === 'AC') {
+      result = false
       clear()
       display('', 0)
-    } else {
-      if (clickEqual) {    // 已经点击过'='
-        prevData = clickEqual
-        values.push(prevData)
-        clickEqual = false
+    }
+    else if (err && /\D/.test(clickValue)) 
+      return
+    else {
+      err = false
+      if (result) {        // 处理是否使用上一次的结果来继续进行运算的情况
+        if (/\D/.test(clickValue)) {
+          prevData = result
+          values.push(prevData)
+        } 
+        result = false
       }
-      if (!((prevData === '0') && (clickValue === '0'))) {
-        // todo 不应该在这里处理显示
-        values.push(clickValue)
-        // let stringValue = values.join('')
-        // stringValue = stringValue.replace(/^0+(\d)/, '$1')
-        // display(stringValue, clickValue)
-        display(values.join(''), clickValue)
-        handleInput(clickValue)
+      if (!((prevData === '0') && (clickValue === '0'))) {          // 处理连续输入多个0开头的数的情况
+        if (!((/\.$/.test(prevData)) && (clickValue === '.'))) {    // 处理连续输入多个'.'的情况
+          if ((prevData === '0') && /[1-9]/.test(clickValue))      // 处理输入了0开头之后输入的是非0的数字的情况
+            values.pop()
+          handleAbnormal(abnormal.ContinuousOperation, clickValue)
+          values.push(clickValue)
+          console.log('values')
+          console.log(values)
+          handleInput(clickValue)
+        }
       }
-      
-      console.log('values')
-      console.log(values)
     }
   })
 })
 
 const handleInput = value => {
   if (/[\d.]/.test(value)) {
-    if (prevData === '')
+    if (!prevData)
       prevData = value
     else
       prevData += value
   } else {
     prevData = Number(prevData)
     if (value === '=') {
-      prevCalculate()
-      let result = calculate()
-      result = result.toFixed(12).replace(/\.0+|0+$/, '')
-      display(`${values.join('')}${result}`, result)
-      clear()
-      clickEqual = result
+      if (/[\+\-×÷]=/.test(values.join('')))
+        handleAbnormal(abnormal.IgnoreOperation)
+      if (!result)        // 处理连续点击'='的情况
+        calculateAndFormat()
     } else {
-      if ((value === '÷') || (value === '×'))
-        tmp.push(prevData, value)
-      else {
+      if (/[+×÷]|\-(?!\d)/.test(values[0]))
+        handleAbnormal(abnormal.AddZero)
+      if ((value === '÷') || (value === '×')) {
+        if (tmp.length) {     // 处理连续乘除的情况
+          prevCalculate()
+          tmp.push(data.pop(), value)
+        } else
+          tmp.push(prevData, value)
+      } else {
         operation.push(value)
         prevCalculate()
       }
     }
-    prevData = 0
+    prevData = false
   }
 
+  if (!err)
+    display()
   console.log('data')
   console.log(data)
   console.log('operation')
   console.log(operation)
 }
 
+const calculateAndFormat = () => {
+  prevCalculate()
+  result = calculate()
+  if (Number.isNaN(result) || !Number.isFinite(result))
+    handleAbnormal(abnormal.DivisorIs0)
+  else {
+    if (!Number.isInteger(result)) 
+      result = result.toFixed(12).replace(/\.0+$|0+$/, '')
+    result = result.toString()
+  }
+}
+
 const calculate = () => {
   if (!operation.length)
-    return data[data.length - 1]
+    return data[0]
   let result
   for (let i = 1; i < data.length; i++) {
     switch (operation[i - 1]) {
@@ -90,16 +121,63 @@ const prevCalculate = () => {
     data.push(prevData)
 }
 
+const handleAbnormal = (cases, clickValue) => {
+  switch(cases) {
+    case abnormal.AddZero: 
+      prevData = 0
+      values = [prevData, ...values]
+      break
+    case abnormal.IgnoreOperation:
+      const ingore = values.splice(values.length - 2, 1)
+      if (/×|÷/.test(ingore)) {
+        data.push(tmp[0])
+        tmp = []
+      } else
+        operation.pop()
+      break
+    case abnormal.ContinuousOperation:
+      const stringTmp = values.join('')
+      if ((/[\+\-×÷]$/.test(stringTmp)) && (/[\+\-×÷]/.test(clickValue))) {
+        values.pop()
+        if (tmp.length) {
+          prevData = tmp[0]
+          tmp = []
+        } else {
+          operation.pop()
+          prevData = data.pop()
+        }
+      }
+      break
+    case abnormal.DivisorIs0:
+      err = true
+      values.pop()
+      display(values.join(''), 'Divisor Cannot Be 0')
+      clear()
+  }
+}
+
 const clear = () => {
   values = []
   data = []
   operation = []
   tmp = []
   prevData = ''
-  clickEqual = false
 }
 
-const display = (displayValue, inputValue) => {
-  $('.display').text(displayValue)
+const display = (...args) => {
+  let stringValue, inputValue
+  if (args.length) {
+    stringValue = args[0]
+    inputValue = args[1]
+  } else {
+    stringValue = values.join('')
+    inputValue = stringValue.match(/\D$|\d+\.*\d*$/)[0]
+    if (result) {
+      stringValue += result
+      inputValue = result
+      clear()
+    }
+  }
+  $('.display').text(stringValue)
   $('.input').text(inputValue)
 }
